ace910e583d9e22b326f2ed302b312195aabf9b6
dindasensih <dindasensi08@gmail.com>
2022-09-29 13:26:10 +0700

    dinda

---
+++ home.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perpustakaan </title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-light">
+        <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="assets/book-icon.png" alt="Logo" width="30" height="24">
+    </a>
+          <a class="navbar-brand" href="#">Perpustakaan 7</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+              <b><a class="nav-link active aria-current="page" href="#">Home</a></b>
+              </li>
+              <li class="nav-item">
+                <b><a class="nav-link" href="#" >Peminjaman</a></b>
+              </li>
+              <li class="nav-item">
+                <b><a class="nav-link" href="#">Pengembalian</a></b>
+              </li>
+              <li class="nav-item">
+                <div class="id=button"></div>
+              <b><button type="button" class="btn btn-primary text-white" style="font-weight:600; width: 100px;">Log Out</button></b>
+              </li>
+          </div>
+        </div>
+      </nav>
+            <!-- ISI content -->
+        <div class="container mt-3">
+        <h3 class="text-center">DAFTAR BUKU</h3>
+        <a href="create.php" class="btn btn-primary">Tambah Buku</a>
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Penulis</th>
+                    <th>Tahun Terbit</th>
+                    <th>Judul Buku</th>
+                    <th>Kota</th>
+                    <th>Penerbit</th>
+                    <th>Cover</th>
+                    <th>Sinopsis</th>
+                    <th>Stock</th>
+
+                </tr>
+            </thead>
+            <tbody>
+          
+            </tbody>
+        </table>
+    </div>
+        </div>
+    </div>
+        
+
+
+
+
+
+   
+
+</body>
+</html>
